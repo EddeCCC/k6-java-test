bfa10827b00aa91edd33b153fbc2282e2e10e124
EddeCCC <eduard.schander@gmx.de>
2022-09-28 14:35:30 +0200

    add CLRunner

--- src/main/java/poc/loadtest/CLRunner.java
+++ src/main/java/poc/loadtest/CLRunner.java
@@ -1,6 +1,39 @@
 package poc.loadtest;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
 public class CLRunner {
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("### LOAD TEST STARTED ###");
+        String url = "src\\main\\resources\\scripts\\lightTest.js";
+        String output = "src\\main\\resources\\output\\output.csv";
+        executeTest(url, output);
+    }
 
+    private void executeTest(String url, String output) throws IOException, InterruptedException {
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec("k6 run " + url + " --out csv=" + output);
+        checkIfProcessFinished(process);
+    }
+
+    private void checkIfProcessFinished(Process process) throws InterruptedException {
+        boolean continueLoop = true;
+        while(continueLoop) {
+            try {
+                process.exitValue();
+                continueLoop = false;
+            } catch (Exception e) {
+                Thread.sleep(3000);
+                System.out.println("...");
+            }
+        }
+        System.out.println("Load test finished with value " + process.exitValue());
+    }
 }
