f33b84962e0b27538ffe55a213d821882cdb635c
EddeCCC <eduard.schander@gmx.de>
2022-09-30 17:30:52 +0200

    delete test annotation

--- src/test/java/api/JavaK6ApplicationTests.java
+++ src/test/java/api/JavaK6ApplicationTests.java
@@ -2,12 +2,9 @@ package api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class JavaK6ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
