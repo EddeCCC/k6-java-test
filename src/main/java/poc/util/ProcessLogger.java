77912c62e737b2313ac1212aa40abf3b4ef97d37
EddeCCC <eduard.schander@gmx.de>
2022-11-07 13:28:48 +0100

    add error logging

--- src/main/java/poc/util/ProcessLogger.java
+++ src/main/java/poc/util/ProcessLogger.java
@@ -4,6 +4,7 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class ProcessLogger {
@@ -17,7 +18,9 @@ public class ProcessLogger {
 
         System.out.println("### LOGGER FINISHED ###");
         this.waitForProcess(process);
-        System.out.println("Load test finished with value " + process.exitValue());
+
+        if(process.exitValue() == -1) this.logError(process);
+        else System.out.println("Load test finished with exitValue " + process.exitValue());
     }
 
     private void waitForProcess(Process process) throws InterruptedException {
@@ -27,5 +30,13 @@ public class ProcessLogger {
             Thread.sleep(3000);
             System.out.println("...");
         }
+    }
+
+    private void logError(Process process) throws IOException {
+        InputStream inputStream = process.getErrorStream();
+        String errorMessage = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        System.out.println("Load test failed with exitValue " + process.exitValue());
+        System.out.println("Message: ");
+        System.err.println(errorMessage);
     }
 }
